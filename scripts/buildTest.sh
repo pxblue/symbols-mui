0c3322eb70f0fd98512a1cd050c50d1abf3454f6
joebochill <boyle.p.joseph@gmail.com>
2020-02-13 10:41:50 -0500

    Update build checks

--- scripts/buildTest.sh
+++ scripts/buildTest.sh
@@ -5,18 +5,18 @@ GRAY='\033[1;30m'
 NC='\033[0m' # No Color
 
 echo "Checking for required files (MUI build)..."
-echo -ne "  package.json: "
-if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  readme: "
-if [ ! -f ./README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  license: "
-if [ ! -f ./LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  metadata (optional): "
-if [ ! -f ./index.json ]; then echo -e "${GRAY}Not Found${NC}"; else echo -e "${GREEN}Found${NC}"; fi
-
 echo -ne "  symbols folder: "
 if [ ! -s ./symbols ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-
+echo -ne "  index: "
+if [ ! -f ./symbols/index.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  types: "
+if [ ! -f ./symbols/index.d.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  package.json: "
+if [ ! -f ./symbols/package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  readme: "
+if [ ! -f ./symbols/README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  license: "
+if [ ! -f ./symbols/LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 
 echo -e "\r\n${GREEN}-----------------------------------"
 echo -e "MUI package successfully created"
