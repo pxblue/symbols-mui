ab365d09b20bbbe3d2a2a9cbc7d9a3d7736b5850
joebochill <boyle.p.joseph@gmail.com>
2021-03-29 16:21:59 -0400

    Re-scaffold with react 17 and latest dependencies

--- demos/showcase/src/index.tsx
+++ demos/showcase/src/index.tsx
@@ -2,24 +2,24 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
-import * as serviceWorker from './serviceWorker';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-//@ts-ignore
+import { CssBaseline, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import * as PXBThemes from '@pxblue/react-themes';
 import '@pxblue/react-themes/open-sans';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-    <MuiThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
-        <CssBaseline />
-        <App />
-    </MuiThemeProvider>,
+    <React.StrictMode>
+        <ThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
